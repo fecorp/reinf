0db1daea26d3c6f70836765652fd57f9b01c82ea
Mariane <a.marianesantana@gmail.com>
2017-09-25 16:15:34 -0300

    Ajustes na tela de Cadastro

---
+++ src/main/java/br/com/fecorp/reinf/view/AcoesBotao.java
@@ -0,0 +1,16 @@
+
+package br.com.fecorp.reinf.view;
+
+public class AcoesBotao {
+
+	
+	    private String productId; 
+	 
+	    public String getProductId() {
+	        return productId;
+	    }
+	 
+	    public void setProductId(String productId) {
+	        this.productId = productId;
+	    }
+}
