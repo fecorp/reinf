8740de5ec2c9951cd994074dc1afcc6a6bda66e2
Mariane <a.marianesantana@gmail.com>
2017-09-26 15:59:35 -0300

    Classes

---
+++ src/main/java/br/com/fecorp/reinf/model/Empresa.java
@@ -0,0 +1,30 @@
+package br.com.fecorp.reinf.model;
+
+public class Empresa {
+	public String empresa;
+	public String filial;
+	public String dataAtualizacao;
+	
+	
+	public String getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	public String getFilial() {
+		return filial;
+	}
+	public void setFilial(String filial) {
+		this.filial = filial;
+	}
+	public String getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+	public void setDataAtualizacao(String dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+	
+	
+
+}
