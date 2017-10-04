66d411b03f0b73589714ce12d1b31f6dcf3657f2
Roger Amorim <roger@fecorp.com.br>
2017-10-04 16:02:47 -0300

    commit

---
+++ src/main/java/br/com/fecorp/reinf/model/ProcessosADMJud.java
@@ -0,0 +1,105 @@
+package br.com.fecorp.reinf.model;
+
+public class ProcessosADMJud {
+
+	public String empresa;
+	public String filial;
+	public String tipoProcesso;
+	public String numProcAdmJud;
+	public double dataDecisaoSent_DespAdm;
+	public double codIndicativoSuspe;
+	public String indiSuspeExigi;
+	public String indiDepMonteIntegral;
+	public String idUniFederacao_UFJud;
+	public double codigoMunicipio;
+	public String codigoIdVara;
+	public double indiAutoriaJud;
+	public String openFlex01;
+	public String openFlex02;
+	
+	public String getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	public String getFilial() {
+		return filial;
+	}
+	public void setFilial(String filial) {
+		this.filial = filial;
+	}
+	public String getTipoProcesso() {
+		return tipoProcesso;
+	}
+	public void setTipoProcesso(String tipoProcesso) {
+		this.tipoProcesso = tipoProcesso;
+	}
+	public String getNumProcAdmJud() {
+		return numProcAdmJud;
+	}
+	public void setNumProcAdmJud(String numProcAdmJud) {
+		this.numProcAdmJud = numProcAdmJud;
+	}
+	public double getDataDecisaoSent_DespAdm() {
+		return dataDecisaoSent_DespAdm;
+	}
+	public void setDataDecisaoSent_DespAdm(double dataDecisaoSent_DespAdm) {
+		this.dataDecisaoSent_DespAdm = dataDecisaoSent_DespAdm;
+	}
+	public double getCodIndicativoSuspe() {
+		return codIndicativoSuspe;
+	}
+	public void setCodIndicativoSuspe(double codIndicativoSuspe) {
+		this.codIndicativoSuspe = codIndicativoSuspe;
+	}
+	public String getIndiSuspeExigi() {
+		return indiSuspeExigi;
+	}
+	public void setIndiSuspeExigi(String indiSuspeExigi) {
+		this.indiSuspeExigi = indiSuspeExigi;
+	}
+	public String getIndiDepMonteIntegral() {
+		return indiDepMonteIntegral;
+	}
+	public void setIndiDepMonteIntegral(String indiDepMonteIntegral) {
+		this.indiDepMonteIntegral = indiDepMonteIntegral;
+	}
+	public String getIdUniFederacao_UFJud() {
+		return idUniFederacao_UFJud;
+	}
+	public void setIdUniFederacao_UFJud(String idUniFederacao_UFJud) {
+		this.idUniFederacao_UFJud = idUniFederacao_UFJud;
+	}
+	public double getCodigoMunicipio() {
+		return codigoMunicipio;
+	}
+	public void setCodigoMunicipio(double codigoMunicipio) {
+		this.codigoMunicipio = codigoMunicipio;
+	}
+	public String getCodigoIdVara() {
+		return codigoIdVara;
+	}
+	public void setCodigoIdVara(String codigoIdVara) {
+		this.codigoIdVara = codigoIdVara;
+	}
+	public double getIndiAutoriaJud() {
+		return indiAutoriaJud;
+	}
+	public void setIndiAutoriaJud(double indiAutoriaJud) {
+		this.indiAutoriaJud = indiAutoriaJud;
+	}
+	public String getOpenFlex01() {
+		return openFlex01;
+	}
+	public void setOpenFlex01(String openFlex01) {
+		this.openFlex01 = openFlex01;
+	}
+	public String getOpenFlex02() {
+		return openFlex02;
+	}
+	public void setOpenFlex02(String openFlex02) {
+		this.openFlex02 = openFlex02;
+	}
+	
+}
