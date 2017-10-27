afa5ad16a7bf7f7be62e2ca8a34c075b61ca96ad
CALVES <alex@fecorp.com.br>
2017-10-27 18:12:01 -0200

    Dao

--- src/main/java/br/com/fecorp/reinf/DAO/AlunoDao.java
+++ src/main/java/br/com/fecorp/reinf/DAO/AlunoDao.java
@@ -1,34 +1,30 @@
 package br.com.fecorp.reinf.DAO;
 
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
-
 import br.com.fecorp.reinf.model.Aluno;
 
 public class AlunoDao {
 
 	public void saveOrUpdate(Aluno aluno) {
-		EntityManager em = util.HibernateUtil
-				.geteEntityManagerFactory().createEntityManager();
+		EntityManager em = util.HibernateUtil.geteEntityManagerFactory().createEntityManager();
 		try {
 			em.getTransaction().begin();
-
+			System.out.println("transaction started--------------");
 			if (aluno.getId() == null)
 				em.persist(aluno);
 			else
 				em.merge(aluno);
-
+			System.out.println("estou no merge do insert--------------");
 			em.getTransaction().commit();
 		} catch (Exception e) {
 			em.getTransaction().rollback();
 		}
 	}
-
+	
 	public void delete(Aluno aluno) {
-		EntityManager em = util.HibernateUtil
-				.geteEntityManagerFactory().createEntityManager();
+		EntityManager em = util.HibernateUtil.geteEntityManagerFactory().createEntityManager();
 		try {
 			em.getTransaction().begin();
 			Aluno al = em.find(Aluno.class, aluno.getId());
@@ -42,8 +38,7 @@ public class AlunoDao {
 	public List<Aluno> list() {
 		List<Aluno> result = null;
 		try {
-			EntityManager em = util.HibernateUtil
-					.geteEntityManagerFactory().createEntityManager();
+			EntityManager em = util.HibernateUtil.geteEntityManagerFactory().createEntityManager();
 			Query query = em.createQuery("FROM Aluno");
 			result = query.getResultList();
 		} catch (Exception e) {
