dfbcbabc153bd9e7f0ce87c16c50fe7c712b190e
maycon <mayconramalhopereira@outlook.com.br>
2017-11-24 15:50:21 -0200

    Criei as persistencias de dados

--- src/main/java/br/com/fecorp/reinf/DAO/TelaServicosDAO.java
+++ src/main/java/br/com/fecorp/reinf/DAO/TelaServicosDAO.java
@@ -1,7 +1,5 @@
 package br.com.fecorp.reinf.DAO;
 
-
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
@@ -9,31 +7,28 @@ import javax.persistence.Persistence;
 
 import br.com.fecorp.reinf.model.TelaServicos;
 
-
 public class TelaServicosDAO {
-	
+
 	private EntityManagerFactory factory = Persistence.createEntityManagerFactory("PersistenciaPU");
 	private EntityManager em = factory.createEntityManager();
-	
-	
-	
-public boolean inserirServico(TelaServicos servico) {
-	try {
-		em.persist(servico);
-		return true;
-	} catch (Exception e) {
-		e.printStackTrace();
-		return false;
-	}
-}
 
-public boolean deletarServico (TelaServicos servico) {
-	try {
-		em.remove(servico);
-		return true;
-	} catch (Exception e) {
-		e.printStackTrace();
-		return false;
+	public boolean inserirServico(TelaServicos servico) {
+		try {
+			em.persist(servico);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
-}
+
+	public boolean deletarServico(TelaServicos servico) {
+		try {
+			em.remove(servico);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 }
